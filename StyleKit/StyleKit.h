35d0c43489e9a253ccc6696bb3866108fa5da677
Nathan Tannar <nathantannar4@gmail.com>
2017-07-05 23:17:45 -0700

    Initial Commit

---
+++ StyleKit/StyleKit.h
@@ -0,0 +1,19 @@
+//
+//  StyleKit.h
+//  StyleKit
+//
+//  Created by Nathan Tannar on 7/5/17.
+//  Copyright © 2017 Nathan Tannar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for StyleKit.
+FOUNDATION_EXPORT double StyleKitVersionNumber;
+
+//! Project version string for StyleKit.
+FOUNDATION_EXPORT const unsigned char StyleKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StyleKit/PublicHeader.h>
+
+
